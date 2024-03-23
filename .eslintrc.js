4124bb3e6563bf433701e2aaa7df2a5773faac1b
lotus <blightningboltz5@gmail.com>
2024-03-24 01:03:33 +0700

    Fixing linting error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,30 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
     es6: true,
-    "jest/globals": true,
+    'jest/globals': true,
+    node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended","prettier"],
+  extends: ['eslint:recommended', 'plugin:react/recommended','prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "jest"],
+  plugins: ['react', 'jest'],
   rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
-    eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
-    "no-console": "error",
-    "react/prop-types": 0,
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 'error',
+    'react/prop-types': 0,
   },
-};
+}
