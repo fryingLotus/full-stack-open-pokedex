58763112cb38bd6845dd1100131a5568c0938dea
lotus <blightningboltz5@gmail.com>
2024-03-24 15:46:35 +0700

    Test if user can go to a pokemon page

--- e2e-tests/example.spec.ts
+++ e2e-tests/example.spec.ts
@@ -6,4 +6,8 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('pokemon page can be opened', async ({ page }) => {
+    await page.goto('/pokemon/bulbasaur')
+    await expect(page.getByText('bulbasaur')).toBeVisible()
+  })
 })
