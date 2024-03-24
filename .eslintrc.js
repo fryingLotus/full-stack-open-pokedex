0d1ae0487977b8fd1833fa71cfd0612d4e7ac0ee
lotus <blightningboltz5@gmail.com>
2024-03-24 22:09:05 +0700

    Please fix the eslint..

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,40 @@
+/* eslint-disable quotes */
 module.exports = {
-  env: {
-    browser: true, // Add browser environment
-    node: true,
-    es6: true,
-    'jest/globals': true,
+  "env": {
+    "browser": true, // Add browser environment
+    "node": true,
+    "es6": true,
+    "jest/globals": true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:playwright/jest-playwright'],
-  globals: {
-    globalThis: false,
-    document: false, // Define document as a global variable
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:playwright/jest-playwright"],
+  "globals": {
+    "globalThis": false,
+    "document": false, // Define document as a global variable
   },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    "ecmaVersion": 2018,
+    "sourceType": "module",
   },
-  plugins: ['react', 'jest'],
-  settings: {
-    react: {
-      version: 'detect', // Specify React version
+  "plugins": ["react", "jest"],
+  "settings": {
+    "react": {
+      "version": "detect", // Specify React version
     },
   },
-  rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    eqeqeq: 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 'error',
-    'react/prop-types': 0,
+  "rules": {
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "never"],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { "before": true, "after": true }],
+    "no-console": "error",
+    "react/prop-types": 0,
   },
 }
 
