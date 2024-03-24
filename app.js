466d192efc1cb59a208b13c1e8c0d3b91da015a3
lotus <blightningboltz5@gmail.com>
2024-03-24 18:51:19 +0700

    Deploying

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-import { express } from 'express'
+const express = require('express')
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-
-app.listen(PORT)
+app.listen(PORT) 
---
+++ build_step.sh
@@ -0,0 +1,13 @@
+
+
+echo "Build script"
+
+# Install dependencies
+npm install
+
+# Execute the build script
+npm run build
+
+# Execute the start-prod script
+npm run start-prod
+
